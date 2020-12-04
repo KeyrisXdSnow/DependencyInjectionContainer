d008fd77b3c3a0a43db44fcfe4f4e36ee34d3672
Keyris <KeyrisXdSnow>
2020-12-05 00:57:02 +0300

    add life cycle

---
+++ Dependency Injection Container/ClassCreator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Dependency_Injection_Container
+{
+    public class ClassCreator
+    {
+        public static object CreateClass(Type type, DependenciesConfiguration configuration)
+        {
+            // TODO : поменять тип exception
+            if (type.GetConstructors().Length == 0) throw new Exception();
+
+            var constructor = SortConstructors(type.GetConstructors(), configuration)[0];
+            var arguments = GetArguments(constructor.GetParameters(), configuration);
+
+            return constructor.Invoke(arguments);
+        }
+        
+        private static object[] GetArguments(IReadOnlyCollection<ParameterInfo> parameterInfos,
+            DependenciesConfiguration configuration)
+        {
+            var arguments = new List<object>(parameterInfos.Count);
+            foreach (var parameter in parameterInfos)
+            {
+                if (configuration.Dependencies.ContainsKey(parameter.ParameterType))
+                {
+                    // TODO : пока получаем только 1 реализацию, потом исправить на лист
+                    var valueType = configuration.Dependencies[parameter.ParameterType][0].Type;
+                    arguments.Add(CreateClass(valueType, configuration));
+                }
+                else
+                {
+                    arguments.Add(null);
+                }
+            }
+
+            return arguments.ToArray();
+        }
+
+        private static List<ConstructorInfo> SortConstructors
+            (IEnumerable<ConstructorInfo> constructorInfos, DependenciesConfiguration configuration)
+        {
+            var @interfaces = configuration.Dependencies.Keys.ToList();
+            var dictionary = new Dictionary<ConstructorInfo, int>();
+
+            foreach (var constructor in constructorInfos)
+            {
+                var dependencyAmount = constructor.GetParameters()
+                    .Count(param => interfaces.Contains(param.ParameterType));
+                dictionary.Add(constructor, dependencyAmount);
+            }
+
+            return dictionary.OrderBy(pair => pair.Value)
+                .ToDictionary(pair => pair.Key, pair => pair.Value).Keys.ToList();
+        }
+    }
+}
---
+++ Dependency Injection Container/DependencyProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Dependency_Injection_Container.exceptions;
+
+namespace Dependency_Injection_Container
+{
+    public class DependencyProvider
+    {
+        private DependenciesConfiguration Configuration { get; }
+        
+        public DependencyProvider(DependenciesConfiguration configuration)
+        {
+            try
+            {
+                ValidateConfiguration(configuration);
+                Configuration = configuration;
+            }
+            catch (ValidationException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+        }
+
+        // TInterface — любой ссылочный тип данных
+        // TImplementation — не абстрактный класс, совместимый с TDependency, объект которого может быть создан.
+        private static void ValidateConfiguration(DependenciesConfiguration configuration)
+        {
+            foreach (var keyValuePair in configuration.Dependencies)
+            {
+                foreach (var implementations in keyValuePair.Value.Where(implementations => implementations.Type.IsAbstract))
+                {
+                    throw new ValidationException(implementations + " is abstract class");
+                }
+            }
+        }
+        
+        public  TInterface Resolve<TInterface>()
+        where TInterface : class
+        {
+            return (TInterface) Resolve(Configuration.Dependencies[typeof(TInterface)][0]);
+        }
+
+        private object Resolve(Dependency dependency)
+        {
+            var result = ClassCreator.CreateClass(dependency.Type, Configuration);
+            
+            if (dependency.LifeCycle == LifeCycle.Singleton) 
+                return dependency.GetInstance(result);
+            
+            return result;
+        }
+        
+    }
+}
